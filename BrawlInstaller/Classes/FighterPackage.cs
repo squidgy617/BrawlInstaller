397d535a5d7d75f1f1d3d8a903682455988a90cf
Ryan Theroux <ryantheroux@yahoo.com>
2024-03-06 20:17:55 -0600

    Updated FighterPackage class to support costumes better.

--- BrawlInstaller/Classes/FighterPackage.cs
+++ BrawlInstaller/Classes/FighterPackage.cs
@@ -4,68 +4,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BrawlLib.Internal;
 
 namespace BrawlInstaller.Classes
 {
+    // TODO: This class does NOT contain options. We will use a different class to store selected options when installing a character package, and their choices
+    //will result in an instance of this class being created. This class is used both when extracting and installing characters.
     public class FighterPackage
     {
-        public List<MultiCosmeticGroup> CSPs { get; set; }
-        public List<CosmeticGroup> BPs { get; set; }
-        public List<CosmeticGroup> CSSIcons { get; set; }
-        public List<CosmeticGroup> StockIcons { get; set; }
-        public List<CosmeticGroup> ReplayIcons { get; set; }
-        public List<FranchiseIcon> FranchiseIcons { get; set; }
-        public List<FighterPath> FighterFolders { get; set; }
-        public List<FighterPath> ExConfigFolders { get; set; }
-        public List<FighterPath> Modules { get; set; }
-        public List<FighterPath> Soundbanks { get; set; }
-        public List<FighterPath> CreditsThemes { get; set; }
-        public List<FighterPath> VictoryThemes { get; set; }
-        public FighterSettings FighterSettings { get; set; }
+        public List<Costume> Costumes { get; set; }
+        public List<Cosmetic> CSSIcons { get; set; }
+        public List<Cosmetic> ReplayIcons { get; set; }
+        public FranchiseIcon FranchiseIcon { get; set; }
+        public List<string> FighterFiles { get; set; }
+        public List<string> ItemFiles { get; set; }
+        public string FighterConfig { get; set; }
+        public string Module { get; set; }
+        public string Soundbank { get; set; }
+        public string CreditsTheme { get; set; }
+        public string VictoryTheme { get; set; }
+        public FighterSettings FighterSettings { get; set; } = null;
     }
 
-    public class FighterOption
+    // TODO: Order of costumes dictates order on CSS/in Ex Config. Internal order dictates order in BRRES. ColorSmashGroup dictates what cosmetics are color smashed.
+    public class Costume
     {
-        public string Name { get; set; } = "Default";
-        public string Description { get; set; } = "The default option";
+        public List<Cosmetic> Cosmetics { get; set; }
+        public List<string> PacFiles { get; set; }
+        public BrawlExColorID Color { get; set; }
     }
 
-    // If there are multiple cosmetic groups with the same Style, user will be allowed to choose between the two
-    public class CosmeticGroup : FighterOption
-    {
-        public CosmeticType CosmeticType { get; set; }
-        public string Style { get; set; }
-        public List<string> Images { get; set; }
-        public List<string> HDImages { get; set; }
-        public List<string> NameImages { get; set; }
-        public List<string> HDNameImages { get; set; }
-    }
-
-    public class MultiCosmeticGroup : FighterOption
+    public class Cosmetic
     {
         public CosmeticType CosmeticType { get; set; }
         public string Style { get; set; }
-        public List<ColorSmashGroup> ColorSmashGroups { get; set; }
-        public List<string> NameImages { get; set; }
-        public List<string> HDNameImages { get; set; }
+        public string Image { get; set; }
+        public string HDImage { get; set; }
+        public string NameImage { get; set; }
+        public string HDNameImage { get; set; }
+        public int? ColorSmashGroup { get; set; }
+        public int? InternalOrder { get; set; }
     }
 
-    public class ColorSmashGroup
-    {
-        public List<string> Images { get; set; }
-        public List<string> HDImages { get; set; }
-    }
-
-    public class FranchiseIcon : FighterOption
+    public class FranchiseIcon
     {
         public string Image { get; set; }
         public string TransparentImage { get; set; }
         public string Model { get; set; }
-    }
-
-    public class FighterPath : FighterOption
-    {
-        public string Path { get; set; }
     }
 
     public class FighterSettings
