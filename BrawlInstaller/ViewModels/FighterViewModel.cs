5a057900909095723aa087b5bcf72b4f3e19eec5
Ryan Theroux <ryantheroux@yahoo.com>
2024-03-24 16:41:18 -0500

    Added BP viewer

--- BrawlInstaller/ViewModels/FighterViewModel.cs
+++ BrawlInstaller/ViewModels/FighterViewModel.cs
@@ -10,6 +10,7 @@ using BrawlLib.SSBB.ResourceNodes;
 using System.Diagnostics;
 using BrawlInstaller.Services;
 using BrawlInstaller.Classes;
+using static BrawlInstaller.ViewModels.FighterViewModel;
 
 namespace BrawlInstaller.ViewModels
 {
@@ -18,16 +19,16 @@ namespace BrawlInstaller.ViewModels
         ICommand LoadCommand { get; }
         void LoadFighter();
         FighterPackage FighterPackage { get; }
-        List<Costume> Costumes { get; }
-        Costume SelectedCostume { get; set; }
+        List<CostumeView> Costumes { get; }
+        CostumeView SelectedCostume { get; set; }
     }
 
     [Export(typeof(IFighterViewModel))]
     internal class FighterViewModel : ViewModelBase, IFighterViewModel
     {
         // Private properties
-        private List<Costume> _costumes;
-        private Costume _selectedCostume;
+        private List<CostumeView> _costumes;
+        private CostumeView _selectedCostume;
 
         // Services
         IExtractService _extractService { get; }
@@ -52,8 +53,8 @@ namespace BrawlInstaller.ViewModels
 
         // Properties
         public FighterPackage FighterPackage { get; set; }
-        public List<Costume> Costumes { get => _costumes; set { _costumes = value; OnPropertyChanged(); } }
-        public Costume SelectedCostume { get => _selectedCostume; set { _selectedCostume = value; OnPropertyChanged(); } }
+        public List<CostumeView> Costumes { get => _costumes; set { _costumes = value; OnPropertyChanged(); } }
+        public CostumeView SelectedCostume { get => _selectedCostume; set { _selectedCostume = value; OnPropertyChanged(); } }
 
         // Methods
         public void LoadFighter()
@@ -67,7 +68,45 @@ namespace BrawlInstaller.ViewModels
                 CosmeticConfigId = 35,
                 CSSSlotConfigId = 35
             });
-            Costumes = FighterPackage.Costumes;
+            Costumes = new List<CostumeView>();
+            foreach(var costume in FighterPackage.Costumes)
+            {
+                Costumes.Add(new CostumeView
+                {
+                    CSPs = new CosmeticView
+                    {
+                        AvailableStyles = costume.Cosmetics.Where(x => x.CosmeticType == Enums.CosmeticType.CSP).Select(x => x.Style).Distinct().ToList(),
+                        SelectedStyle = costume.Cosmetics.Where(x => x.CosmeticType == Enums.CosmeticType.CSP).Select(x => x.Style).Distinct().ToList().First(),
+                        Cosmetics = costume.Cosmetics.Where(x => x.CosmeticType == Enums.CosmeticType.CSP).ToList()
+                    },
+                    BPs = new CosmeticView
+                    {
+                        AvailableStyles = costume.Cosmetics.Where(x => x.CosmeticType == Enums.CosmeticType.BP).Select(x => x.Style).Distinct().ToList(),
+                        SelectedStyle = costume.Cosmetics.Where(x => x.CosmeticType == Enums.CosmeticType.BP).Select(x => x.Style).Distinct().ToList().First(),
+                        Cosmetics = costume.Cosmetics.Where(x => x.CosmeticType == Enums.CosmeticType.BP).ToList()
+                    },
+                    PacFiles = costume.PacFiles,
+                    Color = costume.Color,
+                    CostumeId = costume.CostumeId
+                });
+            }
         }
+    }
+
+    // Mappings
+    public class CostumeView
+    {
+        public CosmeticView CSPs { get; set; }
+        public CosmeticView BPs { get; set; }
+        public List<string> PacFiles { get; set; }
+        public byte Color { get; set; }
+        public int CostumeId { get; set; }
+    }
+
+    public class CosmeticView
+    {
+        public List<string> AvailableStyles { get; set; }
+        public string SelectedStyle { get; set; }
+        public List<Cosmetic> Cosmetics { get; set; }
     }
 }
