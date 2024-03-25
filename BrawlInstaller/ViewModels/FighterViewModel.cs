e838391c7665b587fb7f0fc269dbfab682d8a8ee
Ryan Theroux <ryantheroux@yahoo.com>
2024-03-24 19:56:08 -0500

    Update styles when fighter is loaded

--- BrawlInstaller/ViewModels/FighterViewModel.cs
+++ BrawlInstaller/ViewModels/FighterViewModel.cs
@@ -71,7 +71,7 @@ namespace BrawlInstaller.ViewModels
 
         // Properties
         public FighterPackage FighterPackage { get; set; }
-        public List<Costume> Costumes { get => _costumes; set { _costumes = value; OnPropertyChanged(); } }
+        public List<Costume> Costumes { get => _costumes; set { _costumes = value; OnPropertyChanged(); OnPropertyChanged(nameof(CosmeticOptions)); OnPropertyChanged(nameof(Styles)); } }
         public Costume SelectedCostume { get => _selectedCostume; set { _selectedCostume = value; OnPropertyChanged(); OnPropertyChanged(nameof(SelectedCosmetic)); } }
         public List<CosmeticType> CosmeticOptions { get => _cosmeticOptions; set { _cosmeticOptions = value; OnPropertyChanged(); } }
         public CosmeticType SelectedCosmeticOption { get => _selectedCosmeticOption; set { _selectedCosmeticOption = value; OnPropertyChanged(); OnPropertyChanged(nameof(SelectedCosmetic)); OnPropertyChanged(nameof(Styles)); } }
