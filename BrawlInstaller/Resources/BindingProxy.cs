7bb6223cde331794224ef77e26e938ad1c9a97af
squidgy617 <ryantheroux@yahoo.com>
2024-06-15 23:16:31 -0500

    Added custom validation to franchise ID box

---
+++ BrawlInstaller/Resources/BindingProxy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BrawlInstaller.Resources
+{
+    // https://learn.microsoft.com/en-us/archive/technet-wiki/31422.wpf-passing-a-data-bound-value-to-a-validation-rule
+    public class BindingProxy : Freezable
+    {
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+
+        public object Data
+        {
+            get { return (object)GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register(nameof(Data), typeof(object), typeof(BindingProxy), new PropertyMetadata(null));
+    }
+}
---
+++ BrawlInstaller/Validation/FranchiseIconValidationRules.cs
@@ -0,0 +1,43 @@
+﻿using BrawlInstaller.Classes;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BrawlInstaller.Validation
+{
+    public class FranchiseIconsWrapper : DependencyObject
+    {
+        public static readonly DependencyProperty FranchiseIconsProperty = 
+            DependencyProperty.Register(nameof(FranchiseIcons), typeof(TrackedList<Cosmetic>), typeof(FranchiseIconsWrapper), new FrameworkPropertyMetadata(new TrackedList<Cosmetic>()));
+
+        public TrackedList<Cosmetic> FranchiseIcons
+        {
+            get { return (TrackedList<Cosmetic>)GetValue(FranchiseIconsProperty); }
+            set { SetValue(FranchiseIconsProperty, value); }
+        }
+    }
+    public class FranchiseIdRule : ValidationRule
+    {
+        public FranchiseIconsWrapper Wrapper { get; set; }
+        public FranchiseIdRule()
+        {
+
+        }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var valid = int.TryParse((string)value, out int id);
+
+            if (!valid || this.Wrapper.FranchiseIcons.Items.Any(x => x.Id == id))
+            {
+                return new ValidationResult(false, "Franchise icon ID already in use!");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
