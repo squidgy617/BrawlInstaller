9eaec53aaa42b08a43c8b3194abf8c81550dd560
squidgy617 <ryantheroux@yahoo.com>
2024-07-25 20:15:50 -0500

    Use a tabcontrol for stage list pages

--- BrawlInstaller/ViewModels/Stage/StageListViewModel.cs
+++ BrawlInstaller/ViewModels/Stage/StageListViewModel.cs
@@ -21,7 +21,6 @@ namespace BrawlInstaller.ViewModels
         // Private properties
         private List<StageList> _stageLists;
         private StageList _selectedStageList;
-        private StagePage _selectedPage;
 
         // Services
         IStageService _stageService { get; }
@@ -32,11 +31,12 @@ namespace BrawlInstaller.ViewModels
             _stageService = stageService;
 
             StageLists = _stageService.GetStageLists();
+
+            SelectedStageList = StageLists.FirstOrDefault();
         }
 
         // Properties
         public List<StageList> StageLists { get => _stageLists; set { _stageLists = value; OnPropertyChanged(nameof(StageLists)); } }
         public StageList SelectedStageList { get => _selectedStageList; set { _selectedStageList = value; OnPropertyChanged(nameof(SelectedStageList)); } }
-        public StagePage SelectedPage { get => _selectedPage; set { _selectedPage = value; OnPropertyChanged(nameof(SelectedPage)); } }
     }
 }
