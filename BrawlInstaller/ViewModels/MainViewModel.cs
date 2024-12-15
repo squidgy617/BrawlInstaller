ba933d28c255d5a773f6d00b525cb07c6d31c6ae
squidgy617 <ryantheroux@yahoo.com>
2024-12-15 15:15:56 -0600

    Disable tabs if settings don't exist

--- BrawlInstaller/ViewModels/MainViewModel.cs
+++ BrawlInstaller/ViewModels/MainViewModel.cs
@@ -12,6 +12,9 @@ using BrawlInstaller.Services;
 using BrawlInstaller.Classes;
 using System.Windows.Threading;
 using System.Windows;
+using System.IO;
+using CommunityToolkit.Mvvm.Messaging;
+using static BrawlInstaller.ViewModels.MainControlsViewModel;
 
 namespace BrawlInstaller.ViewModels
 {
@@ -25,27 +28,50 @@ namespace BrawlInstaller.ViewModels
     {
         // Services
         IDialogService _dialogService;
+        ISettingsService _settingsService;
+        IFileService _fileService;
 
         // Importing constructor tells us that we want to get instance items provided in the constructor
         [ImportingConstructor]
         // Order of viewmodels in this constructor determines the order they load in!
-        public MainViewModel(IDialogService dialogService, IMainControlsViewModel mainControlsViewModel, ISettingsViewModel settingsViewModel,
+        public MainViewModel(IDialogService dialogService, ISettingsService settingsService, IFileService fileService, IMainControlsViewModel mainControlsViewModel, ISettingsViewModel settingsViewModel,
             IFighterViewModel fighterViewModel, IStageViewModel stageViewModel)
         {
             _dialogService = dialogService;
+            _settingsService = settingsService;
+            _fileService = fileService;
             MainControlsViewModel = mainControlsViewModel;
             SettingsViewModel = settingsViewModel;
             FighterViewModel = fighterViewModel;
             StageViewModel = stageViewModel;
 
             Application.Current.DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler(AppDispatcherUnhandledException);
+
+            WeakReferenceMessenger.Default.Register<UpdateSettingsMessage>(this, (recipient, message) =>
+            {
+                UpdateSettings();
+            });
+
+            WeakReferenceMessenger.Default.Register<SettingsSavedMessage>(this, (recipient, message) =>
+            {
+                UpdateSettings();
+            });
         }
+
+        // Properties
+        public bool BuildSettingsExist { get => _fileService.FileExists(Path.Combine(_settingsService.AppSettings.BuildPath, "BuildSettings.json")); }
 
         // Viewmodels
         public IMainControlsViewModel MainControlsViewModel { get; set; }
         public ISettingsViewModel SettingsViewModel { get; set; }
         public IFighterViewModel FighterViewModel { get; set; }
         public IStageViewModel StageViewModel { get; set; }
+
+        //Methods
+        private void UpdateSettings()
+        {
+            OnPropertyChanged(nameof(BuildSettingsExist));
+        }
 
         // Global error handler
         void AppDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
