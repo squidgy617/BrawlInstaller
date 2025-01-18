e704aabd633f4c598b9d7a7437b869d4cc2c669b
squidgy617 <ryantheroux@yahoo.com>
2025-01-18 12:19:50 -0600

    Display images when hovering over checklist items

--- BrawlInstaller/ViewModels/Dialogs/CheckListViewModel.cs
+++ BrawlInstaller/ViewModels/Dialogs/CheckListViewModel.cs
@@ -25,6 +25,11 @@ namespace BrawlInstaller.ViewModels
     {
         // Private properties
         private IEnumerable<CheckListItem> _checkListItems;
+        private CheckListItem _hoveredItem;
+
+        // Commands
+        public ICommand ChangeHoveredItemCommand => new RelayCommand(param => ChangeHovered((CheckListItem)param));
+        public ICommand ClearHoveredItemCommand => new RelayCommand(param => ClearHovered());
 
         // Importing constructor
         [ImportingConstructor]
@@ -35,5 +40,19 @@ namespace BrawlInstaller.ViewModels
 
         // Properties
         public IEnumerable<CheckListItem> CheckListItems { get => _checkListItems; set { _checkListItems = value; OnPropertyChanged(nameof(CheckListItems)); } }
+        public CheckListItem HoveredItem { get => _hoveredItem; set { _hoveredItem = value; OnPropertyChanged(nameof(HoveredItem)); } }
+
+        // Methods
+        private void ChangeHovered(CheckListItem checkListItem)
+        {
+            HoveredItem = checkListItem; 
+            OnPropertyChanged(nameof(HoveredItem));
+        }
+
+        private void ClearHovered()
+        {
+            HoveredItem = null;
+            OnPropertyChanged(nameof(HoveredItem));
+        }
     }
 }
