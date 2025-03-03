673bbed9085442d03f031497f2d842f0020a2dda
squidgy617 <ryantheroux@yahoo.com>
2025-03-03 07:44:50 -0600

    Fix trophies being loaded incorrectly when multiple fighters have the same slot ID

--- BrawlInstaller/Services/FighterService.cs
+++ BrawlInstaller/Services/FighterService.cs
@@ -2890,7 +2890,7 @@ namespace BrawlInstaller.Services
                 // TODO: hardcoded code name - might be necessary with the way the codes currently are, but can we get around this?
                 var aliases = _codeService.GetCodeAliases(trophyFileText, "Clone Classic & All-Star Result Data V1.21 [ds22, Dantarion, DukeItOut]");
                 // Get alias for fighter's slot ID
-                var slotAlias = aliases.FirstOrDefault(x => int.TryParse(x.Value.Replace("0x", ""), NumberStyles.HexNumber, null, out int id) && id == slotId);
+                var slotAlias = aliases.FirstOrDefault(x => x.Name.Contains("Slot") && int.TryParse(x.Value.Replace("0x", ""), NumberStyles.HexNumber, null, out int id) && id == slotId);
                 if (slotAlias != null)
                 {
                     // Get classic trophy
