1aa9e00341fa4fce3a7b4895ae3c86e99d18d3cd
squidgy617 <ryantheroux@yahoo.com>
2025-03-21 23:52:49 -0500

    Tag name displays in textbox

--- BrawlInstaller/ViewModels/Fighter/FighterFileViewModel.cs
+++ BrawlInstaller/ViewModels/Fighter/FighterFileViewModel.cs
@@ -128,33 +128,28 @@ namespace BrawlInstaller.ViewModels
         public ObservableCollection<FighterInstallOption> InstallOptions { get => FighterPackage?.InstallOptions != null ? new ObservableCollection<FighterInstallOption>(FighterPackage.InstallOptions) : new ObservableCollection<FighterInstallOption>(); }
 
         [DependsUpon(nameof(SelectedPacFile))]
-        public string SelectedSuffix { get => SelectedPacFile?.Suffix; set { SelectedPacFile.Suffix = value; OnPropertyChanged(nameof(SelectedSuffix)); } }
-
-        [DependsUpon(nameof(SelectedSuffix))]
-        public string DisplaySuffix
-        {
-            get
-            {
-                var suffix = SelectedPacFile?.Suffix;
-                if (!string.IsNullOrEmpty(suffix) && suffix.StartsWith("$") && suffix.Substring(1).Length % 2 == 0)
-                {
-                    suffix = suffix.Substring(1);
-                    var newSuffix = string.Empty;
-                    for (int i = 0; i < suffix.Length; i += 4)
-                    {
-                        var hexPair = suffix.Substring(i, 4);
-                        var byteArray = new byte[2];
-                        byteArray[0] = Convert.ToByte(hexPair.Substring(0, 2), 16);
-                        byteArray[1] = Convert.ToByte(hexPair.Substring(2, 2), 16);
-                        newSuffix += Encoding.BigEndianUnicode.GetString(byteArray);
-                    }
-                    return "Tag: " + newSuffix;
-                }
-                return string.Empty;
-            }
-        }
+        public string SelectedSuffix { get => GetDisplaySuffix(SelectedPacFile?.Suffix); set { SelectedPacFile.Suffix = value; OnPropertyChanged(nameof(SelectedSuffix)); } }
 
         // Methods
+        private string GetDisplaySuffix(string suffix)
+        {
+            if (!string.IsNullOrEmpty(suffix) && suffix.StartsWith("$") && suffix.Substring(1).Length % 2 == 0)
+            {
+                suffix = suffix.Substring(1);
+                var newSuffix = string.Empty;
+                for (int i = 0; i < suffix.Length; i += 4)
+                {
+                    var hexPair = suffix.Substring(i, 4);
+                    var byteArray = new byte[2];
+                    byteArray[0] = Convert.ToByte(hexPair.Substring(0, 2), 16);
+                    byteArray[1] = Convert.ToByte(hexPair.Substring(2, 2), 16);
+                    newSuffix += Encoding.BigEndianUnicode.GetString(byteArray);
+                }
+                suffix = "$" + newSuffix;
+            }
+            return suffix;
+        }
+
         public void LoadFighterFiles(FighterLoadedMessage message)
         {
             FighterPackage = message.Value;
